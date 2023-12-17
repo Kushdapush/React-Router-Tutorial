fcff95e1e8108ddcb0c3f61ecd8d5ae4a098002d
Kushagra <22bcs148@iiitdmj.ac.in>
2023-12-18 00:35:52 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root, {
+    loader as rootLoader,
+    action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./ErrorPage";
+import Contact, { loader as contactLoader, } from "./routes/contact";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: rootLoader,
+        action: rootAction,
+        children: [
+            {
+                path: "contacts/:contactId",
+                element: <Contact />,
+                loader: contactLoader,
+            },
+        ],
+    },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
